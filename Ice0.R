fd4be2ffc029d4e0b2bcda955b30a0795b82dd9c
codyxu1 <93735452+codyxu1@users.noreply.github.com>
2021-11-04 16:06:27 -0400

    Add files via upload

---
+++ Ice0.R
@@ -0,0 +1,26 @@
+print("hello world")
+#comment introduce
+x<-3
+y<-7
+xy<-"whatsup"
+print(c(x,y, x+y, xy))
+print(x)
+x<-5
+print(x)
+x<-y
+print(x)
+a=1+2
+print(paste0("1+2=",a))
+b=2-1
+print(paste0("2-1=",b))
+d=9/3
+print(paste0("9/3=",d))
+comment
+m=10%%3
+print(m)
+print(c(paste0("m = ", m), paste0("x = ", x)))
+print(paste0("m is equal to x: ", m == x))
+print(paste0("m is not equal to x: ", m != x))
+print(paste0("m is smaller than x: ", m < x))
+getwd()
+
